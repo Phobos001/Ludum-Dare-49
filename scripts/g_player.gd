a1c860889243d8c9ebd33f05f80408b233207cce
Phobos001 <phobos001@protonmail.com>
2021-10-02 12:56:32 -0500

    Update: Oct 2 12:56PM

--- scripts/g_player.gd
+++ scripts/g_player.gd
@@ -1,19 +1,27 @@
-extends Node2D
+extends Node
 
-onready var rb = $RigidBody2D
-onready var collision = $RigidBody2D/CollisionShape2D
-onready var sprite = $Sprite
+onready var body = $KinematicBody
+onready var collision = $KinematicBody/CollisionShape2D
+onready var sprite = $Sprite3D
+onready var camera = $Camera
+onready var cameramouseray = $Camera/RayCast
 
+export var camera_height = 30
 export var hammer_attack_force: float = 10.0
 
+var mouse_screen_position: Vector2
+var target_camera_position: Vector3
+
+var mouse_delta: Vector2
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_delta = event.relative
+
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
 
-func _physics_process(delta):
-	if Input.is_action_just_pressed("action_attackhammer"):
-		var aim_direction = (get_global_mouse_position() - rb.global_position)
-		rb.apply_central_impulse(aim_direction * hammer_attack_force)
-	if Input.is_action_just_pressed("action_attackgun"):
-		var aim_direction = (get_global_mouse_position() - rb.global_position)
-		
+func _process(delta):
 	pass
+	
